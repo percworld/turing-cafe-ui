40fb66c6150ba53c71155b91ee4edc67cdc3c566
percworld <percworld@gmail.com>
2021-04-07 12:00:12 -0600

    Add event handlers to form

--- src/App/App.js
+++ src/App/App.js
@@ -10,7 +10,9 @@ class App extends Component {
     super();
     this.state = {
       currentReservations: [{ id: 2, name: "Leta", date: "4/5", time: "7:00", number: 2 }],
-      displayHome: true
+      displayHome: true,
+      request: '',
+
     }
   }
 
@@ -19,9 +21,9 @@ class App extends Component {
 
   }
 
-  submitNewReservation = event => {
-    event.preventDefault
 
+  emptyField() {
+    this.setState({ request: '' })
   }
 
   handleCancel() {
--- src/Form/Form.js
+++ src/Form/Form.js
@@ -2,12 +2,68 @@ import React from 'react';
 import './Form.css';
 import { Link } from 'react-router-dom'
 
-export default function Form({ submitRes }) {
+class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: ''
+      date: ''
+      time: ''
+      guests: ''
+    }
+  }
+
+  addReservation = event => {
+    event.preventDefault();
+    this.setState({ id: Date.now(), ...this.state });
+    this.emptyField();
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  render() {
     return (
-        <div className='resyForm'>
-            <Link to='/' />
+      <div className='resyForm'>
+        <Link to='/' />
+        <form>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            value={this.state.name}
+            onChange={event => this.handleChange(event)}
+          />
+          <input
+            type='text'
+            placeholder='Date'
+            name='date'
+            value={this.state.date}
+            onChange={event => this.handleChange(event)}
+          />
+          <input
+            type='text'
+            placeholder='Time'
+            name='time'
+            value={this.state.time}
+            onChange={event => this.handleChange(event)}
+          />
+          <input
+            type='text'
+            placeholder='Number of Guests'
+            name='number'
+            value={this.state.number}
+            onChange={event => this.handleChange(event)}
+          />
+          <button></button>
 
+        </form>
 
-        </div>
+      </div>
     )
+  }
+
 }
+
+export default Form;
